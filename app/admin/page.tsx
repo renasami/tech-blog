8e36eb8474e399bc5d6cb3cebb7bd49783b395d2
renasami <drrr0502@gmail.com>
2023-08-20 00:38:45 +0900

    wip

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+import { Button, Spinner } from "@chakra-ui/react";
+
 const AdminPage = () => {
   return (
-    <div>
-      <h1>Admin Page</h1>
-    </div>
+    <>
+      <div>
+        <h1>Admin Page</h1>
+        <Button>hoge</Button>
+      </div>
+      <Spinner />
+    </>
   );
 };
 
